919fc6d407440bb53cfdefa971a7e5bfd52af949
yangyuan <1367511704@qq.com>
2020-06-14 21:28:45 +0800

    feat(对登录弹窗的修改)

--- src/components/Dialog/LoginDialog.vue
+++ src/components/Dialog/LoginDialog.vue
@@ -61,4 +61,37 @@ export default {
 };
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.login_dialog {
+    .head {
+        width: 100%;
+        .logo {
+            width: 82px;
+            height: 82px;
+            margin: 0 auto;
+            img {
+                width: 100;
+                height: 100%;
+                max-width: 100%;
+            }
+        }
+        .logo_text {
+            width: 100%;
+            text-align: center;
+            margin: 20px 0px;
+            span {
+                font-size: 20px;
+                font-family: MicrosoftYaHei;
+                color: rgba(15, 79, 168, 1);
+                line-height: 26px;
+                -webkit-background-clip: text;
+            }
+        }
+    }
+    .login_btn {
+        button {
+            width: 100%;
+        }
+    }
+}
+</style>
