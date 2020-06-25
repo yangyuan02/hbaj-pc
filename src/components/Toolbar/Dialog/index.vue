0e31efe9835e1e0525374ca58abffe82920f1404
yangyuan <1367511704@qq.com>
2020-06-25 23:22:05 +0800

    feat(添加一个附件管理)

---
+++ src/components/Toolbar/Dialog/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-dialog
+        title="附件管理"
+        :visible.sync="visible"
+        :close-on-click-modal="false"
+        :modal-append-to-body="false"
+        @open="open"
+        @close="close"
+    ></el-dialog>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less"></style>
