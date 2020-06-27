4265c8da8d6fe59ed2bf8408efcf8d6739d2aa35
yangyuan <1367511704@qq.com>
2020-06-27 13:32:34 +0800

    feat(主页添加一个全景图)

--- src/components/common/BgNav.vue
+++ src/components/common/BgNav.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg_nav">
+    <div class="bg_nav" id="pano">
         <Nav></Nav>
     </div>
 </template>
@@ -10,8 +10,26 @@ export default {
     data() {
         return {};
     },
+    methods: {
+        initPano() {
+            this.$nextTick(() => {
+                embedpano({
+                    id: "kr",
+                    swf: `${process.env.VUE_APP_PANO_PATH}/panostatic/tour.swf`,
+                    xml: `${process.env.VUE_APP_PANO_PATH}/panostatic/scene01.xml`,
+                    target: "pano",
+                    html5: "prefer",
+                    mobilescale: 1.0,
+                    passQueryParameters: true
+                });
+            });
+        }
+    },
     components: {
         Nav
+    },
+    mounted() {
+        this.initPano();
     }
 };
 </script>
@@ -24,7 +42,14 @@ export default {
     background: url("./images/nav.png") no-repeat;
     background-size: 100%;
     margin-left: 24px;
-    padding-top: 62px;
-    padding-left: 24px;
+    // padding-top: 62px;
+    // padding-left: 24px;
+    position: relative;
+    nav {
+        position: absolute;
+        left: 24px;
+        top: 62px;
+        z-index: 10;
+    }
 }
 </style>
