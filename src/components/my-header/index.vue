821eefbe314b5bc8804026914b3ece6f78b12a96
yangyuan <1367511704@qq.com>
2020-06-28 22:14:26 +0800

    fix(个人信息页面修改)

---
+++ src/components/my-header/index.vue
@@ -0,0 +1,187 @@
+<!--
+ * @Author: yangyuan
+ * @Date: 2020-04-17 21:43:01
+ * @Email: 1367511704@qq.com
+ * @LastEditTime: 2020-05-18 21:30:13
+ * @Description: 
+ -->
+<template>
+    <div class="my-header-content">
+        <header>
+            <i class="iconfont icontubiao-13 goBack" @click="$router.back(-1)" v-if="isBack"></i>
+            <span>我的</span>
+            <i class="iconfont icontubiao-251 edit" @click="edit" v-if="isEdit"></i>
+        </header>
+        <div class="my-person-info">
+            <div class="person-thumb">
+                <input
+                    v-if="isOnUplaod"
+                    type="file"
+                    @change="uploadAvatar($event)"
+                    class="upload_file"
+                    accept="image/png,image/jpeg,image/jpg"
+                />
+                <img :src="globalConfig.imagePath + info.avatar" alt="" v-if="info.avatar" />
+            </div>
+            <div class="person-digest">
+                <div class="nickname">
+                    <span>{{ info.namecard }}</span>
+                </div>
+                <div class="info">
+                    <span>{{ info.description }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ImageUpload from "@/widget/imageUpload";
+export default {
+    data() {
+        return {};
+    },
+    props: {
+        info: {
+            type: Object,
+            default: {}
+        },
+        onUploadSuccess: {
+            type: Function,
+            default: () => {}
+        },
+        isOnUplaod: {
+            type: Boolean,
+            default: false
+        },
+        isEdit: {
+            type: Boolean,
+            default: true
+        },
+        isBack: {
+            type: Boolean,
+            default: true
+        }
+    },
+    methods: {
+        uploadAvatar(e, opt) {
+            this.$showLoading();
+
+            var file = e.currentTarget.files[0];
+            const info = this.info;
+            var imageUpload = new ImageUpload(file, {
+                url: `/api/file/upload?fileName=${info.namecard}&relatedId=${info.id}&fileType=USER_AVATAR`,
+                fileKey: "file",
+                onUpload: result => {
+                    this.$hideLoading();
+
+                    if (result.suceeded >= 1) {
+                        // this.params[opt.img] = result.data.img_dir
+                        e.target.value = "";
+                        if (this.onUploadSuccess && typeof this.onUploadSuccess === "function") {
+                            this.onUploadSuccess(result.data);
+                        }
+                        console.log(result);
+                    } else {
+                        this.$toast(result.msg);
+                    }
+                },
+                onError: () => {
+                    this.$toast("网络服务器错误");
+                }
+            });
+            imageUpload.start();
+        },
+        edit() {
+            this.$router.push({ name: "mySetting" });
+        }
+    }
+};
+</script>
+
+<style lang="less">
+.my-header-content {
+    width: 100%;
+    height: 4.4rem;
+    background: url("./images/header-bg.png") no-repeat;
+    background-size: 100%;
+    position: relative;
+    header {
+        height: 0.88rem;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: transparent;
+        position: relative;
+        span {
+            color: #fff;
+        }
+        .edit {
+            position: absolute;
+            right: 0;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            color: #fff;
+            font-size: 0.34rem;
+        }
+        .goBack {
+            position: absolute;
+            left: 0.2rem;
+            top: 50%;
+            transform: translateY(-50%);
+            color: #fff;
+            font-size: 0.3rem;
+        }
+    }
+    .my-person-info {
+        display: flex;
+        padding: 0 0.24rem;
+        .person-thumb {
+            width: 1.57rem;
+            height: 1.57rem;
+            border-radius: 50%;
+            background: url("./images/person.png");
+            background-size: 100%;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            img {
+                width: 100%;
+                height: 100%;
+                border-radius: 50%;
+            }
+            .upload_file {
+                width: 100%;
+                height: 100%;
+                position: absolute;
+                left: 0;
+                top: 0;
+                opacity: 0;
+                z-index: 110;
+            }
+        }
+        .person-digest {
+            flex: 1;
+            margin-left: 1.87rem;
+            .nickname {
+                margin-bottom: 0.43rem;
+                span {
+                    font-size: 0.3rem;
+                    font-family: MicrosoftYaHei;
+                    color: rgba(255, 255, 255, 1);
+                    line-height: 0.4rem;
+                }
+            }
+            .info {
+                span {
+                    font-size: 0.24rem;
+                    font-family: MicrosoftYaHei;
+                    color: rgba(255, 255, 255, 1);
+                    line-height: 0.42rem;
+                }
+            }
+        }
+    }
+}
+</style>
--- src/views/my/person/index.vue
+++ src/views/my/person/index.vue
@@ -1,9 +1,63 @@
 <template>
     <div class="message_container">
         <div class="message_info">
-            <Title title="海宝资讯"></Title>
+            <Title title="用户中心"></Title>
             <div class="message_list">
-                <!-- <message-item v-for="(item, index) in [1, 2, 3, 4, 5]" :key="index"></message-item> -->
+                <my-header :info="user"></my-header>
+                <div class="person-box">
+                    <div class="role common" v-for="(item, index) in [1, 2]" :key="index">
+                        <div class="thumb"></div>
+                        <div class="name">
+                            <span>海宝安检</span>
+                        </div>
+                        <div class="tag">
+                            <span>角色</span>
+                        </div>
+                    </div>
+                    <div class="organization common" v-for="(item, index) in [1, 2]" :key="index">
+                        <div class="thumb">
+                            <!-- <img :src="globalConfig.imagePath + item.avatar" alt="" /> -->
+                        </div>
+                        <div class="name">
+                            <span>{{ item.name }}</span>
+                        </div>
+                        <div class="tag">
+                            <span>机构</span>
+                        </div>
+                    </div>
+                    <div class="auth-box" v-for="(item, Pindex) in blocks" :key="Pindex">
+                        <div class="title">
+                            <span class="ellipsis">{{ item.name }}访问权限</span>
+                        </div>
+                        <div class="major-box">
+                            <div class="left-major">
+                                <ul>
+                                    <li
+                                        class="ellipsis"
+                                        v-for="(moduleItem, _moduleIndex) in item.moduleList"
+                                        :key="_moduleIndex"
+                                        :class="[_moduleIndex === ModuleIndex ? 'active' : '']"
+                                        @click="setModuleIndex(Pindex, _moduleIndex)"
+                                    >
+                                        {{ moduleItem.name }}
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="right-major">
+                                <div
+                                    class="item"
+                                    v-for="(itemClass, index) in item.classList"
+                                    :key="index"
+                                >
+                                    <span class="ellipsis">{{ itemClass.name }}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tag">
+                            <span>权限</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <BgNav></BgNav>
@@ -11,9 +65,9 @@
 </template>
 
 <script>
-// import MessageItem from "./item.vue";
 import Title from "@/components/common/Title";
 import BgNav from "@/components/common/BgNav";
+import MyHeader from "@/components/my-header";
 export default {
     data() {
         return {};
@@ -21,7 +75,8 @@ export default {
     components: {
         // MessageItem,
         Title,
-        BgNav
+        BgNav,
+        MyHeader
     }
 };
 </script>
@@ -32,6 +87,153 @@ export default {
     height: 100%;
     .message_info {
         width: 363px;
+        .message_list {
+            .person-box {
+                padding: 14px 17px;
+                background: #fff;
+                min-height: 100%;
+                .common {
+                    display: flex;
+                    align-items: center;
+                    background: rgba(15, 79, 168, 1);
+                    padding: 8px;
+                    position: relative;
+                    .thumb {
+                        width: 60px;
+                        height: 60px;
+                        background: #f0f0f0;
+                        margin-left: 25px;
+                        margin-right: 15px;
+                        border-radius: 5px;
+                        img {
+                            width: 100%;
+                            height: 100%;
+                            border-radius: 5px;
+                            max-width: 100%;
+                        }
+                    }
+                    .name {
+                        font-size: 14px;
+                        font-family: MicrosoftYaHei;
+                        color: rgba(255, 255, 255, 1);
+                        line-height: 19px;
+                        -webkit-background-clip: text;
+                    }
+                    .tag {
+                        width: 65px;
+                        height: 20px;
+                        background: rgba(224, 32, 32, 1);
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        border-radius: 10px 0px 0px 10px;
+                        position: absolute;
+                        right: 0;
+                        top: 8px;
+                        span {
+                            color: #fff;
+                        }
+                    }
+                }
+                .role {
+                    margin-bottom: 8px;
+                    &:last-child {
+                        margin-bottom: 0;
+                    }
+                }
+                .organization {
+                    margin: 8px 0;
+                    .tag {
+                        background: rgba(255, 213, 26, 1);
+                    }
+                }
+                .auth-box {
+                    position: relative;
+                    // width: 6.8rem;
+                    height: 175px;
+                    background: rgba(15, 79, 168, 1);
+                    border-radius: 5px;
+                    padding-left: 10px;
+                    padding-top: 10px;
+                    margin-bottom: 8px;
+                    &:last-child {
+                        margin-bottom: 0rem;
+                    }
+                    .title {
+                        span {
+                            color: #fff;
+                        }
+                    }
+                    .major-box {
+                        display: flex;
+                        margin-top: 10px;
+                        .left-major {
+                            ul {
+                                display: flex;
+                                flex-direction: column;
+                                justify-content: space-around;
+                                height: 100%;
+                                li {
+                                    color: #fff;
+                                    position: relative;
+                                    padding-left: 5px;
+                                    &.active {
+                                        &::before {
+                                            content: "";
+                                            height: 100%;
+                                            width: 2px;
+                                            position: absolute;
+                                            left: 0;
+                                            top: 0;
+                                            background: rgba(255, 157, 59, 1);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        .right-major {
+                            width: 4.63rem;
+                            height: 2.6rem;
+                            background: rgba(0, 145, 255, 1);
+                            border-radius: 0.1rem;
+                            margin-left: 0.16rem;
+                            padding: 0.1rem;
+                            display: flex;
+                            flex-wrap: wrap;
+                            justify-content: space-between;
+                            .item {
+                                width: 1.28rem;
+                                height: 0.4rem;
+                                border-radius: 0.04rem;
+                                border: 0.01rem solid rgba(255, 255, 255, 1);
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                span {
+                                    color: #fff;
+                                    font-size: 0.24rem;
+                                }
+                            }
+                        }
+                    }
+                    .tag {
+                        position: absolute;
+                        width: 1.3rem;
+                        height: 0.4rem;
+                        background: rgba(255, 157, 59, 1);
+                        border-radius: 0.2rem 0px 0px 0.2rem;
+                        right: 0;
+                        top: 0.2rem;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        span {
+                            color: #fff;
+                        }
+                    }
+                }
+            }
+        }
     }
     .message_right {
         flex: 1;
