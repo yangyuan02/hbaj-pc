600da45322c12848d7bfaec1855cd7ee42135ecc
yangyuan <1367511704@qq.com>
2020-08-22 15:59:19 +0800

    fix(修复富文本上传图片地址为服务器地址)

--- src/components/common/RichTextBox.vue
+++ src/components/common/RichTextBox.vue
@@ -14,12 +14,70 @@ export default {
             this.$nextTick(() => {
                 this.editor = new Editor("#editor"); // 构造富文本实例
                 this.editorConfig(); // 修改富文本配置
+                this.upload();
+                this.editor.customConfig.debug = true;
                 this.editor.create(); // 创建富文本
             });
         },
+        upload() {
+            this.editor.customConfig.uploadImgHooks = {
+                before: function(xhr, editor, files) {
+                    // 图片上传之前触发
+                    // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，files 是选择的图片文件
+                    // 如果返回的结果是 {prevent: true, msg: 'xxxx'} 则表示用户放弃上传
+                    // return {
+                    //     prevent: true,
+                    //     msg: '放弃上传'
+                    // }
+                },
+                success: function(xhr, editor, result) {
+                    console.log(result, "result,success");
+                    // 图片上传并返回结果，图片插入成功之后触发
+                    // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
+                },
+                fail: function(xhr, editor, result) {
+                    console.log(result, "result,fail");
+                    // 图片上传并返回结果，但图片插入错误时触发
+                    // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
+                },
+                error: function(xhr, editor) {
+                    console.log(xhr, "result,error");
+                    // 图片上传出错时触发
+                    // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
+                },
+                timeout: function(xhr, editor) {
+                    // 图片上传超时时触发
+                    // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
+                },
+
+                // 如果服务器端返回的不是 {errno:0, data: [...]} 这种格式，可使用该配置
+                // （但是，服务器端返回的必须是一个 JSON 格式字符串！！！否则会报错）
+                customInsert: function(insertImg, result, editor) {
+                    console.log(process.env, "result");
+                    // 图片上传并返回结果，自定义插入图片的事件（而不是编辑器自动插入图片！！！）
+                    // insertImg 是插入图片的函数，editor 是编辑器对象，result 是服务器端返回的结果
+
+                    // 举例：假如上传图片成功后，服务器端返回的是 {url:'....'} 这种格式，即可这样插入图片：
+                    var url = "";
+                    if (process.env.NODE_ENV === "development") {
+                        url = globalConfig.imagePath + result.data.path;
+                    } else {
+                        url = result.data.path;
+                    }
+
+                    insertImg(url);
+
+                    // result 必须是一个 JSON 格式字符串！！！否则报错
+                }
+            };
+        },
         editorConfig() {
             // 富文本配置
-            this.editor.customConfig.uploadImgShowBase64 = true; // 上传图片base64
+            const projectId = this.$route.params.projectId;
+            this.editor.customConfig.uploadImgShowBase64 = false; // 上传图片base64
+            this.editor.customConfig.uploadImgServer = `/api/file/upload?fileName=default&relatedId=${projectId}&fileType=HOTSPOT_HTML`;
+            this.editor.customConfig.uploadFileName = "file";
+
             this.editor.customConfig.pasteIgnoreImg = true; // 过滤直接粘贴进来的图片
             this.editor.customConfig.pasteTextHandle = content => this.pasteTextHandle(content);
             /**
