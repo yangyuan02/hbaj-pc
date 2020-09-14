5c2929bd4c4ff5244b8990e0759afc0f6639a03f
yangyuan <1367511704@qq.com>
2020-09-14 16:39:22 +0800

    fix(去跳转)

--- src/widget/ajax.js
+++ src/widget/ajax.js
@@ -43,7 +43,7 @@ export default function ajax({
                     resolve(xhr.response);
                 } else if (xhr.status === 401) {
                     window.localStorage.removeItem("authorization");
-                    window.location.href = "/";
+                    // window.location.href = "/";
                 } else {
                     resolve({
                         data: [],
