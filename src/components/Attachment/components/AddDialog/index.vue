131c854110a3f81391079f32a81d1d3b122617b9
yangyuan <1367511704@qq.com>
2020-11-09 16:50:30 +0800

    pref: 优化附件上传弹窗

---
+++ src/components/Attachment/components/AddDialog/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <component
+        :is="tag.component"
+        ref="tab-child"
+        v-if="defaultTabName === tag.order"
+        :hotspotId="hotspotId"
+        :id="id"
+        :onSuccess="onSuccess"
+        :visible.sync="visible"
+    />
+</template>
+
+<script>
+import AudioDialog from "./AudioDialog";
+import AudioEngDialog from "./AudioEngDialog";
+import ImageDialog from "./ImageDialog";
+import TextDialog from "./TextDialog";
+import VideoDialog from "./VideoDialog";
+export default {
+    props: {
+        hotspotId: {
+            // 热点id
+            type: [String, Number],
+            required: true
+        },
+        id: {
+            // 具体的某一个id
+            type: [String, Number]
+        },
+        onSuccess: {
+            // 确定之后的回调
+            type: Function,
+            default: () => {}
+        },
+        visible: {
+            type: Boolean,
+            default: false
+        },
+        defaultTabName: {
+            // 动态组件的哪一个
+            type: String,
+            default: "1"
+        }
+    },
+    data() {
+        return {
+            tabs: [
+                { component: TextDialog, name: "文本弹窗", order: "1", class: "TextDialog" },
+                { component: ImageDialog, name: "图片弹窗", order: "2", class: "ImageDialog" },
+                { component: AudioDialog, name: "音频弹窗", order: "3", class: "AudioDialog" },
+                { component: VideoDialog, name: "视频弹窗", order: "4", class: "VideoDialog" }
+                // { component: RichTextBox, name: "富文本", order: "5", class: "RichTextBox" }
+            ]
+        };
+    }
+};
+</script>
--- src/components/Attachment/components/index.vue
+++ src/components/Attachment/components/index.vue
@@ -1,9 +1,34 @@
 <template>
-    <el-tabs v-model="defaultTabName" class="tag-container">
-        <el-tab-pane :label="tag.name" :key="tag.order" :name="tag.order" v-for="tag in Tabs" :class="tag.class">
-            <component :is="tag.component" ref="tab-child" v-if="defaultTabName === tag.order" :hotspotId="hotspotId" />
-        </el-tab-pane>
-    </el-tabs>
+    <main class="attchment_dialog">
+        <el-tabs v-model="defaultTabName" class="tag-container">
+            <el-tab-pane
+                :label="tag.name"
+                :key="tag.order"
+                :name="tag.order"
+                v-for="tag in Tabs"
+                :class="tag.class"
+            >
+                <component
+                    :is="tag.component"
+                    ref="tab-child"
+                    v-if="defaultTabName === tag.order"
+                    :hotspotId="hotspotId"
+                />
+            </el-tab-pane>
+        </el-tabs>
+        <div class="operate">
+            <el-button type="danger" @click="deleteItem()" v-if="isDelete">删除</el-button>
+            <el-button type="primary" @click="addDialog">添加</el-button>
+            <el-button type="primary" @click="editDialog">修改</el-button>
+        </div>
+        <AddDialog
+            :hotspotId="hotspotId"
+            :id="id"
+            :onSuccess="onSuccess"
+            :defaultTabName="defaultTabName"
+            :visible.sync="showsDialog"
+        />
+    </main>
 </template>
 
 <script>
@@ -12,6 +37,7 @@ import TextList from "./TextList";
 import AudioList from "./AudioList";
 import Video from "./Video";
 import RichTextBox from "./RichTextBox";
+import AddDialog from "./AddDialog";
 export default {
     props: {
         hotspotId: {
@@ -21,11 +47,21 @@ export default {
         orderList: {
             type: Array,
             default: () => ["文本", "图片", "音频", "视频", "富文本"]
+        },
+        engType: {
+            // 是否是专业英语
+            type: Boolean,
+            default: false
         }
+    },
+    components: {
+        AddDialog
     },
     data() {
         return {
             defaultTabName: "1",
+            id: "", // 某一条的id
+            showsDialog: false,
             tabs: [
                 { component: TextList, name: "文本", order: "1", class: "TextList" },
                 { component: ImagesList, name: "图片", order: "2", class: "ImagesList" },
@@ -48,18 +84,50 @@ export default {
             });
             this.defaultTabName = tabs[0].order;
             return tabs;
+        },
+        buttonText() {
+            return "添加";
+        },
+        isDelete() {
+            return true;
+        }
+    },
+    methods: {
+        onSuccess() {
+            // 确定之后的回调
+            console.log("1111");
+        },
+        addDialog() {
+            this.showsDialog = true;
+        },
+        editDialog() {
+            console.log("编辑");
         }
     }
 };
 </script>
 
 <style lang="less" scoped>
-/deep/ .el-tabs__content {
-    max-height: 500px;
-    height: 500px;
-    overflow-y: scroll;
-    .RichTextBox {
-        height: calc(100% - 40px) !important;
+.attchment_dialog {
+    position: relative;
+
+    /deep/ .el-tabs__content {
+        max-height: 500px;
+        height: 500px;
+        overflow-y: scroll;
+        .RichTextBox {
+            height: calc(100% - 40px) !important;
+        }
+    }
+    .operate {
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+    .attach_model {
+        .el-dialog__body {
+            padding-top: 0px;
+        }
     }
 }
 </style>
